7e7718db313dfa645e775890813902a680d79816
eds2109 <warlock609@gmail.com>
2021-10-11 20:06:31 +0300

    add test dynamic content

--- products/views.py
+++ products/views.py
@@ -3,8 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 # Контроллеры = views(вьюхи) = функции
 
+
 def index(request):
     return render(request, 'products/index.html')
 
+
 def products(request):
     return render(request, 'products/products.html')
+
+
+# Тестовый контроллер для пеердачи динамики
+def test_context(request):
+    context = {
+        'title': 'Store',
+        'header': 'Добро пожаловать!',
+        'username': 'Иван Иванов',
+        'products': [
+
+        ],
+    }
+    return render(request, 'products/test-context.html', context)
