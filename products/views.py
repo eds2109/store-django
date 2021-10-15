6e21454a2d72d7f6294c04b76ad8cd86de0423f0
eds2109 <warlock609@gmail.com>
2021-10-15 19:51:34 +0300

    add dynamic output of categories and products

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from products.models import ProductCategory, Product
 
 # Create your views here.
 # Контроллеры = views(вьюхи) = функции
@@ -13,25 +14,9 @@ def index(request):
 
 def products(request):
     context = {
-        'title' : 'Store - Каталог'
+        'title': 'Store - Каталог',
+        'categories': ProductCategory.objects.all(),
+        'products': Product.objects.all(),
     }
     return render(request, 'products/products.html', context)
 
-
-# Тестовый контроллер для пеердачи динамики
-def test_context(request):
-    context = {
-        'title': 'store',
-        'header': 'Добро пожаловать!',
-        'username': 'Иван Иванов',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090.00},
-            {'name': 'Синяя куртка The North Face', 'price': 23725.00},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390.00},
-        ],
-        #'promotion': True,
-        'products_of_promotion': [
-            {'name' : 'Черный рюкзак Nike Heritage', 'price' : 2340.00},
-        ]
-    }
-    return render(request, 'products/test-context.html', context)
