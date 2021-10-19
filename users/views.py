538fb834f18755433ee05b20f3157cceae70e36e
eds2109 <warlock609@gmail.com>
2021-10-19 19:14:11 +0300

    add users base template

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
+from users.forms import UserLoginForm
 
 def login(request):
-    return render(request, 'users/login.html')
+    form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def register(request):
