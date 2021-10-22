84aca5df6759d571aa30a5f5f41c2ee148bf134f
eds2109 <warlock609@gmail.com>
2021-10-22 13:42:32 +0300

    add UserRegistrationForm form

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
@@ -15,5 +15,23 @@ class UserLoginForm(AuthenticationForm):
 
     def __init__(self, *args, **kwargs):
         super(UserLoginForm, self).__init__(*args, **kwargs)
+        for field_name, filed in self.fields.items():
+            filed.widget.attrs['class'] = 'form-control py-4'
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'plceholder': 'Введите имя'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'plceholder': 'Введите фамилию'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Введите имя пользователя'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'Введите адрес эл. почты'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' : 'Введите пароль'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' : 'Подтвердите пароль'}))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
         for field_name, filed in self.fields.items():
             filed.widget.attrs['class'] = 'form-control py-4'
