b96949172d1cb3cafd41679a9c5e5f84db787657
eds2109 <warlock609@gmail.com>
2021-10-23 09:12:19 +0300

    add profile fields

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 def login(request):
     if request.method == 'POST':
@@ -21,7 +21,7 @@ def login(request):
 
 
 def register(request):
-    if request.method == 'POST' :
+    if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
@@ -34,4 +34,12 @@ def register(request):
     return render(request, 'users/register.html', context)
 
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, files=request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'form' : form}
+    return render(request, 'users/profile.html', context)
